2b2b1efbaa7237be9d45e48883439a0fd156d2c5
Indigo Lily <yulex.42@gmail.com>
2017-04-30 17:25:51 -0300

    Renamed, make hue based on how many times drawn instead of frame count

--- walker.js
+++ walker.js
@@ -2,6 +2,7 @@ function walker(x, y) {
     this.loc = createVector(x, y);
     this.prevLoc = this.loc;
     this.speed = width/100;
+    this.hue = 0;
     this.move = function() {
         this.prevLoc = createVector(this.loc.x, this.loc.y);
         var v = createVector(this.speed, 0);
@@ -18,8 +19,9 @@ function walker(x, y) {
     this.draw = function() {
         push();
         colorMode(HSB);
-        stroke(frameCount/5%360, 100, 100);
+        stroke(this.hue%360, 100, 100);
         line(this.prevLoc.x, this.prevLoc.y, this.loc.x, this.loc.y);
+        this.hue += .0025;
         pop();
     }
 }
